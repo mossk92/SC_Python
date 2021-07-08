fc2c1058124fb1faf8cd4251fc4c407415884268
mossk92 <86653337+mossk92@users.noreply.github.com>
2021-07-08 17:23:20 +1000

    Variables and User Input Exercises complete

---
+++ var-user-input.py
@@ -0,0 +1,81 @@
+#Q1) Additional of Two User Input Numbers
+print("Sum of User Inputs")
+
+no1 = input("   Enter a number: ")
+no2 = input("   Enter another number: ")
+sumcalc = float(no1) + float(no2)
+print(f"      {float(sumcalc)}")
+print()
+print()
+
+no1 = input("   Enter a - number: ")
+no2 = input("   Enter another number: ")
+sumcalc = float(no1) + float(no2)
+print(f"      {float(sumcalc)}")
+print()
+print()
+
+no1 = input("   Enter a number (decimal): ")
+no2 = input("   Enter another - number: ")
+sumcalc = float(no1) + float(no2)
+print(f"      {float(sumcalc)}")
+print()
+print()
+
+#Q2) Mulitplication of Two User Input Numbers
+print("Multiplication of User Inputs")
+
+no3 = input("   Enter a number: ")
+no4 = input("   Enter another number: ")
+multcalc = float(no3) * float(no4)
+print(f"      {float(multcalc)}")
+print()
+print()
+
+no3 = input("   Enter a - number: ")
+no4 = input("   Enter another number: ")
+multcalc = float(no3) * float(no4)
+print(f"      {float(multcalc)}")
+print()
+print()
+
+no3 = input("   Enter a number (decimal): ")
+no4 = input("   Enter another - number: ")
+multcalc = float(no3) * float(no4)
+print(f"      {float(multcalc)}")
+print()
+print()
+
+#Q3) Distance Calculation
+print("Distance Calculation")
+
+no5 = input("   How many kilometres? ")
+con_m = float(no5) * 1000
+con_cm = float(no5) * 100000
+print(f"      {no5}km = {int(con_m)}m")
+print(f"      {no5}km = {int(con_cm)}cm")
+print()
+print()
+
+no5 = input("   How many kilometres (with decimal)? ")
+con_m = float(no5) * 1000
+con_cm = float(no5) * 100000
+print(f"      {no5}km = {int(con_m)}m")
+print(f"      {no5}km = {int(con_cm)}cm")
+print()
+print()
+
+#Q4) Summary of Inputs
+print("Summary of Inputs")
+
+name = input("   What is your name? ")
+height = input("   How tall are you (cm)? ")
+print(f"      {name} is {height}cms tall")
+print()
+print()
+
+name = input("   What is your name? ")
+height = input("   How tall are you (cm)? ")
+print(f"      {name} is {height}cms tall")
+print()
+print()
