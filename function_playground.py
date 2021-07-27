4389191ebf1baf1fa5761425b856d9f363652a59
mossk92 <86653337+mossk92@users.noreply.github.com>
2021-07-27 17:48:08 +1000

    Exercises Clean Up

---
+++ function_playground.py
@@ -0,0 +1,114 @@
+print("CLASSWORK")
+print("-----------------------------------------------")
+
+print("Simple Function:")
+
+def create_greeting(name):
+        #this is the name of the function that you can call later. Inside the brackets is a variable to define later
+    greeting = f"Hello {name}"
+    return greeting
+        #return is similar to print however will not actually show anything on the screen unless the function is called
+
+print(create_greeting("Chilli"))
+print(create_greeting("Ivy"))
+print(create_greeting("Remus"))
+
+print()
+print("Function to take integer and multiply by 3:")
+print("      Option 1: Ask for input")
+
+def ask_integer():
+        #this option asks for an input and multiplies that input
+    integer = int(input("How old are you? "))
+    return integer
+
+print(ask_integer()*3)
+
+print("      Option 2: Print Input")
+
+def time_three(num):
+    return num*3
+
+print(time_three(10))
+
+print("      Option 3: ")
+
+print("      Option 4: ")
+
+print()
+print("Function to Convert:")
+
+def convert_cm_to_inch(length_cm):
+    length_in_inch = length_cm / 2.54
+    return length_in_inch
+
+print(convert_cm_to_inch(20))
+
+def convert_inch_to_cm(length_in):
+    length_in_cm = length_in * 2.54
+    return length_in_cm
+
+print(convert_inch_to_cm(7.87))
+
+print()
+print("Add Function:")
+
+def add(a,b):
+    return a+b
+
+print(add(2,3))
+
+print()
+print("Mean Function:")
+
+def mean(a,b):
+    return (a + b) / 2
+
+print(mean(5,15))
+
+print()
+print("EXERCISES")
+print("-----------------------------------------------")
+
+print("Q1: Write a function that take a temperature in f and return the temp in c")
+
+def temp_in_cel(temp_in_f):
+    temp_in_cel = (temp_in_f - 32) * (5/9)
+    return temp_in_cel
+
+print(temp_in_cel(32))
+
+print()
+print("Q2: Write function that accepts one parameter (integer) and return Tree when it is odd or False when even")
+
+def is_odd(num):
+    if num % 2 == 0: 
+            # % = modulo which looks for remainder when divided (essentially decimal)
+            # If there is no remainder when divided by 2, it means the number is even
+        return False
+    else:
+        return True
+
+print(is_odd(2))
+
+print()
+print("Q3: Write a function that reutrn the mean of a list of numbers")
+
+def mean(num_list):
+    return sum(num_list) / len(num_list)
+
+print(mean([4,3,2,6]))
+
+print(mean([10,5,6]))
+
+print()
+print("Q4: Write a function that takes two parameters; unit price and # of units and returns the total cost as a string")
+
+def total_cost(price,quantity):
+    return price * quantity
+
+print(f'${total_cost(4.25,3)}')
+
+print()
+print('########################################################')
+print()
---
+++ variables_playground.py
@@ -0,0 +1,133 @@
+print("CLASSWORK")
+print("-----------------------------------------------")
+
+print("String:")
+day = "Saturday"
+month = "July"
+    # Strings are words or phrases
+    # They are contained within "" or ''
+message = f"Today is {day}, {month}"
+
+print(message)
+print(type(day))
+
+print()
+print("Integer:")
+
+apples = 5
+banana = 8
+    # Integers are whole numbers
+    # They are written as is with no formatting
+fruits = apples + banana
+
+print(fruits)
+print(type(fruits))
+
+print()
+print("Float:")
+
+height = 1.70
+weight = 999.9
+    #Floats are decimal numbers
+    #They are shown wth a decimal place
+calculation = weight - height
+print(calculation)
+print(type(calculation))
+
+calculation = height + banana
+print(calculation)
+
+print()
+print("Add Integers:")
+
+#Run distance in m
+run1_dist = 1400
+run2_dist = 1800
+
+total_dist = run1_dist + run2_dist
+
+print(total_dist)
+
+print()
+print("Add Floats:")
+
+#Run Distance in km
+run3_dist = 1.7
+run4_dist = 1.35
+
+total_dist = run3_dist + run4_dist
+
+print(total_dist)
+
+print()
+print("Division:")
+
+m_dist = run1_dist / run2_dist
+print(m_dist)
+print(run1_dist / 1000)
+print(run3_dist * 1000)
+print(type(m_dist))
+    #Result of division will always by a float regardless of if answer is whole or not
+
+print()
+print("Comparing different types:")
+
+var1 = "banana"
+var2 = 5
+var3 = "3"
+var4 = 10
+
+#str + int
+#print(var1 + var2)
+    #will fail as python cannot cross type
+
+#use int as string
+print(var1 + str(var2))  
+print(var1 + var3)  
+
+#use string as int
+print(var2 + int(var3))
+
+#multiply str by int
+print(var1 * var2)
+    #duplicates string by the amount of the integer
+
+#int to float
+print(float(var2))
+print(type(var2))
+
+#Divisions are float regardless of inputs
+calculation = var4 / var2
+print(type(var4))
+print(type(var2))
+print(calculation)
+print(type(calculation))
+
+print()
+
+print()
+print("EXERCISES")
+print("-----------------------------------------------")
+
+print("Q1: ")
+
+
+
+print()
+print("Q2: ")
+
+
+
+print()
+print("Q3: ")
+
+
+
+print()
+print("Q4: ")
+
+
+
+print()
+print('########################################################')
+print()
